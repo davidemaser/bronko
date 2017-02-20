73883aa620a0b755e123128ae6f052b221989ea5
David Maser <davidemaser@gmail.com>
2017-02-20 17:12:35 -0500

    Add documentation

--- src/bits/ajax.js
+++ src/bits/ajax.js
@@ -6,6 +6,9 @@ ajax.init=function(obj){
     /*
     pass object in the following format
     {url:'the_url',name:'unique name for dataset',key:'optional key for data',split:false}
+    if you add the split parameter to the object, the data will be saved into the
+    packets object. IT WILL NOT however be split into usable packet sizes. To do so
+    use the data.split function after the data has been loaded
      */
     if(typeof obj == 'object'){
         var type = obj.split == true ? 'packets' : 'data';
